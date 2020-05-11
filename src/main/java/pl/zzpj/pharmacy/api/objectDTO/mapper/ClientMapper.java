ae8b9e399d4db11b8b88a4cacf552005fe6adba6
Motwg <38580353+Motwg@users.noreply.github.com>
2020-05-11 11:49:55 +0200

    Merge

--- src/main/java/pl/zzpj/pharmacy/api/objectDTO/mapper/ClientMapper.java
+++ src/main/java/pl/zzpj/pharmacy/api/objectDTO/mapper/ClientMapper.java
@@ -5,38 +5,28 @@ import pl.zzpj.pharmacy.api.objectDTO.ClientDTO;
 
 public class ClientMapper {
 
-    public static Client toClient(ClientDTO clientDTO) {
+    public static Client toClient(ClientDTO clientDTO) { //, Set<Order> orders) {
         return Client.builder()
                      .id(clientDTO.getId())
                      .address(clientDTO.getAddress())
                      .firstName(clientDTO.getFirstName())
                      .lastName(clientDTO.getLastName())
+                     //.orders(orders)
+                     //.prescriptions(clientDTO.getPrescriptions())
                      .build();
-                // TO DO
-                /*
-                .id(clientDTO.getId())
-                .firstName(clientDTO.getFirstName())
-                .lastName(clientDTO.getLastName())
-                .address(clientDTO.getAddress())
-                .orders(clientDTO.getOrders())
-                .prescriptions(clientDTO.getPrescriptions())
-                */
     }
 
     public static ClientDTO toClientDTO(Client client) {
+        /*Set<OrderDTO> orders = client.getOrders()
+                .stream()
+                .map(order -> OrderMapper.toOrderDTO(order))
+                .collect(Collectors.toSet());*/
         return ClientDTO.builder()
                         .firstName(client.getFirstName())
                         .lastName(client.getLastName())
                         .address(client.getAddress())
-                        .id(client.getId()).build();
-                // TO DO
-                /*
-                .id(client.getId())
-                .firstName(client.getFirstName())
-                .lastName(client.getLastName())
-                .address(client.getLastName())
-                .orders(client.getOrders())
-                .prescriptions(client.getPrescriptions())
-                 */
+                        .id(client.getId())
+                        //.orders(orders)
+                        .build();
     }
 }
